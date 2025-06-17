b47ee0f6ac394a5440ec8b2f2dabc21d380b0460
Singh-Ishu <itzshaansingh@gmail.com>
2025-06-17 18:57:28 +0530

    UPDATE the table in the sidebar component

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,72 +1,72 @@
 function Sidebar() {
     return (
         <div className="sidebar-main">
+            <h1>System Statistics</h1>
             <div className="table_component" role="region" tabIndex="0">
                 <table>
-                    <thead>
-                        <tr>
-                            <th colSpan="2">System Statistics</th>
-                        </tr>
-                    </thead>
                     <tbody>
-                        <section className="sidebar-stats-section">
-                            <tr>
-                                <td>Total Nodes</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Active Nodes</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Down Nodes</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Unreported Nodes</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Demand Unmet Nodes</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Demand Met Nodes</td>
-                                <td></td>
-                            </tr>
-                        </section>
-                        <section className="sidebar-stats-section">
-                            <tr>
-                                <td>Total Flow</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Ideal Flow</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Flow Difference</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Pressure Stats</td>
-                                <td></td>
-                            </tr>
-                        </section>
-                        <section className="sidebar-stats-section">
-                            <tr>
-                                <td>Current Leak Locations</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Most Vulnerable pipe</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>% of Sensors reporting</td>
-                                <td></td>
-                            </tr>
-                        </section>
+                        <tr>
+                            <td>Total Nodes</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Active Nodes</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Down Nodes</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Unreported Nodes</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Demand Unmet Nodes</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Demand Met Nodes</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Total Flow</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Ideal Flow</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Flow Difference</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Pressure Stats</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Current Leak Locations</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Most Vulnerable Pipe</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>% of Sensors Reporting</td>
+                            <td></td>
+                        </tr>
                     </tbody>
                 </table>
             </div>

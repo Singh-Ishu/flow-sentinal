94f8892d9298152c8ab5eec07320075b1f3e66f9
Singh-Ishu <itzshaansingh@gmail.com>
2025-06-20 19:27:30 +0530

    UPDATE Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import MaintenancePage from './pages/MaintenancePage';
+import LeakPreventionPage from './pages/LeakPreventionPage';
+import Navbar from "./components/Navbar";
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <Navbar/>
+            <main style={{ padding: '2rem', background: '#f7f7f7', minHeight: '100vh' }}>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/maintenance" element={<MaintenancePage />} />
+                    <Route path="/leak-prevention" element={<LeakPreventionPage />} />
+                </Routes>
+            </main>
+        </>
+    );
 }
 
 export default App;

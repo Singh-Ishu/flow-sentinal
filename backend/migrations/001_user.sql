cd2b5acd016386b93a01eded97e1138f63afbe1b
Ishaan Singh <itzshaansingh@gmail.com>
2026-06-25 04:40:44 +0530

    Fix update_updated_at_column() creation

--- backend/migrations/001_user.sql
+++ backend/migrations/001_user.sql
@@ -10,6 +10,19 @@ CREATE TABLE IF NOT EXISTS "user" (
     "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_task_updated_at
+    BEFORE UPDATE ON "task"
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at_column();
+
 CREATE TRIGGER update_user_updated_at
     BEFORE UPDATE ON "user"
     FOR EACH ROW
--- backend/migrations/004_task.sql
+++ backend/migrations/004_task.sql
@@ -15,15 +15,3 @@ CREATE TABLE IF NOT EXISTS "task" (
     "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
-
-CREATE TRIGGER update_task_updated_at
-    BEFORE UPDATE ON "task"
-    FOR EACH ROW
-    EXECUTE FUNCTION update_updated_at_column();
